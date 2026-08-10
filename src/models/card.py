e91e4244f502e2b1298b098b965016766b34cc79
Linus <linus.postcrossing@mail.de>
2026-08-10 21:51:12 +0200

    Fix docstrings and class inheritance

--- src/models/card.py
+++ src/models/card.py
@@ -2,32 +2,31 @@ from dataclasses import dataclass #dataclasses are classes for storing data (duh
 
 @dataclass
 class Card:
-"""Basic class for all Cards"""
+    """Basic class for all Cards
+name: the displayed name
+card_type: either Pokemon or Trainer"""
     name: str
     card_type: str
 
 @dataclass
-class Pokemon:
-"""Class tracking all stats that makes a Pokemon unique"""
-#some inheritance shenanigans
+class Pokemon(Card):
+    """Class tracking all stats that makes a Pokemon unique.
+hp: current health points. start at max, decrease when damaged.
+damage: amount of hp enemy pokemon lose if they get attacked
+ex pokemon give two points when KO'd.
+attached_energy: amount of energy tokens (currency required to unleash attacks) are currently equipped. starts at 0 and increases as game progresses."""
     hp: int
     damage: int
     is_ex: bool = False
     attached_energy: int = 0
+    card_type : str = "Pokemon"
 
     def __repr__(self):
-        return f"Pokemon('{self.name}{if self.is_ex: (ex)}': HP = {self.hp}, damage = {self.damage}, attached energy = {self.attached_energy}"
+        return f"Pokemon('{self.name}{'(ex)' if self.is_ex else ''}': HP = {self.hp}, damage = {self.damage}, {'attached energy = {self.attached_energy}' if self.attached_energy else 'no energy'}"
 
 @dataclass
-class Trainer:
-"""Trainer cards are non-Pokemon cards. Only one Supporter card may be played per turn"""
-#again inheritance
+class Trainer(Card):
+    """Trainer cards are non-Pokemon cards. Only one Supporter card may be played per turn"""
     is_supporter: bool = False
     description: str #will implement later
-
-@dataclass
-class Energyzone: #correct file?
-"""Energy types are set based on the deck. Unused Energy is lost and can't be 'saved' for later."""
-    energy_type: str
-    next_type: str
-    empty: bool = False
+    card_type: str = "Trainer"
