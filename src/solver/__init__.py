11afb7470e139cb8183c2f34f598e8a08e2f29bb
Linus <linus.postcrossing@mail.de>
2026-09-08 17:21:05 +0200

    Added move ordering for more efficient pruning

---
+++ src/solver/__init__.py
@@ -0,0 +1,3 @@
+"""
+Finds the optimal action in a given GameState by browsing through all possible moves in a tree.
+"""
---
+++ src/solver/move_ordering.py
@@ -0,0 +1,36 @@
+def order_moves(state: GameState, moves: list[str]) -> list[str]:
+
+    """
+Order moves by priority for better pruning:
+
+Attacks are the most impactful.
+Using your ressources is always good.
+Attaching Energy is important.
+Retreating is situational.
+Ending Turn is a last resort.
+    """
+
+    # Categorize moves
+    attacks = []
+    plays = []
+    attaches = []
+    retreats = []
+    others = []
+    end_turn = []
+    
+    for move in moves:
+        if move.startswith("ATTACK_"):
+            attacks.append(move)
+        elif move.startswith("PLAY_CARD_"):
+            plays.append(move)
+        elif move.startswith("ATTACH_ENERGY_"):
+            attaches.append(move)
+        elif move.startswith("RETREAT_"):
+            retreats.append(move)
+        elif move == "END_TURN":
+            end_turn.append(move)
+        else:
+            others.append(move)
+    
+    # Return in priority order
+    return attacks + plays + attaches + retreats + others + end_turn
